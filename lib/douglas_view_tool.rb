b8ff9b672dcf64bf5e5f33db68aa89fdf369715b
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 15:38:36 +0000

    Dev tool initial commit

---
+++ lib/douglas_view_tool.rb
@@ -0,0 +1,6 @@
+require "douglas_view_tool/version"
+require "douglas_view_tool/renderer"
+
+module DouglasViewTool
+  # Your code goes here...
+end
---
+++ lib/douglas_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DouglasViewTool
+  VERSION = "0.1.0"
+end
